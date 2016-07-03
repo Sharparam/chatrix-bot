2549a3dc00c30e7dd04c0413090210bf9a434d8e
Adam Hellberg <sharparam@sharparam.com>
2016-07-04 00:10:21 +0200

    Document errors.rb

--- lib/chatrix/bot/errors.rb
+++ lib/chatrix/bot/errors.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 module Chatrix
   class Bot
+    # General bot error.
     class BotError < RuntimeError
     end
 
+    # Error raised while parsing a command.
     class CommandError < BotError
     end
 
