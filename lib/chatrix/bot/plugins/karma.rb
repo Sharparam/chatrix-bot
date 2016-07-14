71327de24bdc8a45617ef3349acd592cda638f20
Adam Hellberg <sharparam@sharparam.com>
2016-07-14 05:02:48 +0200

    Add karma plugin

---
+++ lib/chatrix/bot/plugins/karma.rb
@@ -0,0 +1,32 @@
+module Chatrix
+  class Bot
+    module Plugins
+      class Karma < Plugin
+        CHANGES = { '++' => 1, '--' => -1 }
+
+        register_pattern(/\A([@\w:\.]+)(\+\+|--)\z/, :karma)
+
+        def karma(room, message, match)
+          user = match[1]
+          update_karma user, CHANGES[match[2]]
+          room.messaging.send_message "#{user}'s karma is #{self[user]}."
+        end
+
+        private
+
+        def [](user)
+          @config[user] ||= 0
+        end
+
+        def []=(user, value)
+          @config[user] = value
+          @config.save
+        end
+
+        def update_karma(user, change)
+          self[user] += change
+        end
+      end
+    end
+  end
+end
